ca9b0aa31458a8dc344a8e00d5a1418fc2ae2e93
Adriana Martinez <adrisanzm03@gmail.com>
2025-05-31 19:14:16 -0600

    Proyecto

---
+++ index.js
@@ -0,0 +1,26 @@
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+
+const empleado = require('./empleado');
+const user = require('./user');
+
+const auth = require('./Middleware/auth');
+const notFound = require('./Middleware/notFound');
+const index = require('./Middleware/Index');
+const cors = require('./Middleware/cors');
+
+app.use(cors);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', index);
+app.use('/user', user);
+app.use(auth);
+app.use('/empleado', empleado); 
+app.use(notFound);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is Running...');
+});
